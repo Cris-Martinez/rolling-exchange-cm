6fe9df8727dc7e6ae55d73111940b0687e03fae0
Cristian Esteban Martinez <cristianestebanmartinez888@gmail.com>
2020-07-28 18:03:59 -0300

    configuration the App.js

--- App.js
+++ App.js
@@ -6,23 +6,23 @@ import CurrenciesContainer from './src/components/content/CurrenciesContainer'
 import BottomContainer from './src/components/BottonContainer'
 import FavoritesSearchbar from './src/components/favorites/FavoritesSearchbar'
 import FavoritesContainer from './src/components/favorites/FavoritesContainer'
-import MainContainer from './src/components/pages/MainContainer'
 import currencies from './src/constants/currencies'
 
 import { darkTheme as defaultTheme } from './src/constants/colors'
 import { darkTheme } from './src/constants/colors'
 import { lightTheme } from './src/constants/colors'
 
+const windowHeigh = Dimensions.get('window').height
+
 export default function App() {
   const [ mainVisible, setMainVisible ] = useState(true)
-  const [ fromCurrency, setFromCurrency ] = useState('usd')
+  const [ fromCurrency, setFromCurrency ] = useState('ars')
   const [ amount, setAmount ] = useState('')
   const [ favoriteCurrencies, setFavoriteCurrencies ] = useState([])
   const [ allCurrencies, setAllCurrencies ] =
     useState(currencies.map(curr => ({ ...curr, isFavorite: false })))
   const [ appTheme, setAppTheme ] = useState(defaultTheme)
-
-  const updateTheme = () =>{
+  const updateTheme = () => {
     appTheme.name === 'darkTheme' ? setAppTheme(lightTheme) : setAppTheme(darkTheme)
   }
 
@@ -39,54 +39,55 @@ export default function App() {
 
   return (
     <PaperProvider>
-      {Platform.OS === 'ios' && <StatusBar barStyle="default" />}
-      {Platform.OS === 'android' && <View style={getStyle(appTheme, 'statusBarUnderlay')} />}
+      <View style={{ minHeight: windowHeigh }}>
+        {Platform.OS === 'ios' && <StatusBar barStyle="default" />}
+        {Platform.OS === 'android' && <View style={getStyle(appTheme, 'statusBarUnderlay')} />}
 
-      {
-        mainVisible ?
-        (
-          <Fragment>
-            {/* <MainContainer/> */}
-            <TopContainer 
-                      appTheme={appTheme}
-                      fromCurrency={fromCurrency}
-                      setFromCurrency={setFromCurrency}
-                      amount={amount} 
-                      setAmount={setAmount}/>
-            <CurrenciesContainer 
-                      appTheme={appTheme} 
-                      changeScreen={setMainVisible} 
-                      fromCurrency={fromCurrency}
-                      amount={amount} 
-                      allCurrencies={allCurrencies}/>
-            <BottomContainer 
-                      appTheme={appTheme} 
-                      updateTheme={updateTheme}/>
-          </Fragment>
-        )
-        :
-        (
-          <Fragment>
-            <FavoritesSearchbar appTheme={appTheme} changeScreen={setMainVisible} />
-            <FavoritesContainer 
-                        appTheme={appTheme}
-                        allCurrencies={allCurrencies}
-                        addFavoriteCurrency={addFavoriteCurrency}
-                        updateCurrency={updateCurrency}/>
-          </Fragment>
-        )
-      }
-      
+        {
+          mainVisible ?
+          (
+            <Fragment>
+              <CurrenciesTop
+                appTheme={appTheme}
+                fromCurrency={fromCurrency}
+                setFromCurrency={setFromCurrency}
+                amount={amount}
+                setAmount={setAmount}
+              />
+              <CurrenciesContainer
+                appTheme={appTheme}
+                fromCurrency={fromCurrency}
+                amount={amount}
+                changeScreen={setMainVisible}
+                allCurrencies={allCurrencies}
+              />
+              <CurrenciesBottom appTheme={appTheme} updateTheme={updateTheme} />
+            </Fragment>
+          )
+          :
+          (
+            <Fragment>
+              <FavoritesTop appTheme={appTheme} changeScreen={setMainVisible} />
+              <FavoritesContainer
+                appTheme={appTheme}
+                allCurrencies={allCurrencies}
+                addFavoriteCurrency={addFavoriteCurrency}
+                updateCurrency={updateCurrency}
+              />
+            </Fragment>
+          )
+        }
+      </View>
     </PaperProvider>
   );
 }
 
 const getStyle = (theme, component) => {
-    switch(component){
-      case 'statusBarUnderlay':
-        return({
-          height: 28,
-          backgroundColor: theme.secondary,
-        })
-    }
+  switch(component) {
+    case 'statusBarUnderlay':
+      return({
+        height: 28,
+        backgroundColor: theme.secondary,
+      })
+  }
 }
